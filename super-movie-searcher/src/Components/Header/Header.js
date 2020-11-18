3d128f27e0b7a76211142aa442cc76b4aeca37be
Solid-Wolf <thomasliedts@gmail.com>
2020-11-18 13:42:06 +0100

    Add search input to the header and beginning of state

--- super-movie-searcher/src/Components/Header/Header.js
+++ super-movie-searcher/src/Components/Header/Header.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css';
 
 const Header = () => {
+  const [movie, setMovie] = useState('');
+
+  function handleChange(event) {
+    console.log(event.target.value);
+    setMovie(event.target.value);
+  }
+
   return (
     <header>
       <Link className='Header-link' to='/'>
         Accueil
       </Link>
+      <input type='search' className='Header-search' onChange={handleChange} />
     </header>
   );
 };
