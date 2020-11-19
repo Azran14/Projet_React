88dc1a305a7de6f4d349b11da97042a465639663
Oceane <zhou.oceane@gmail.com>
2020-11-19 11:36:01 +0100

    MoviesPage affichage movies trending

--- super-movie-searcher/src/Components/Movie/Movie.js
+++ super-movie-searcher/src/Components/Movie/Movie.js
@@ -1,15 +1,20 @@
 import React from 'react';
 
-const MOVIE_API="https://api.themoviedb.org/3/movie/550?api_key=b15f77463ee209064e6a74ac153c528c";
 
-const Movie = ({original_title, poster_path, release_date, overview, vote_average}) => (
-    <div class='movie-preview'>
-        <img src = {poster_path, MOVIE_API} alt={original_title}/>
-        <div class='movie-information'>
-            <span>{vote_average}</span>
-            <h3>{original_title} {release_date}</h3>
+
+
+const Movie = ({movie}) => {
+    console.log(movie.poster_path, 'poster'); //mettre un string
+    return(
+        <div className='movie-preview'>
+        <img src = {movie.poster_path} alt={movie.original_title}/>
+        <div className='movie-information'>
+            <span>{movie.vote_average}</span>
+            <h3>{movie.original_title} {movie.release_date}</h3>
         </div>
     </div>
-);
+    );
+    
+};
 
 export default Movie;
--- super-movie-searcher/src/Components/MoviesPage/MoviesPage.js
+++ super-movie-searcher/src/Components/MoviesPage/MoviesPage.js
@@ -1,7 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Movie from '../Movie/Movie'
 
-const MoviesPage = () => {
-  return <div>Page de resultat des films</div>;
-};
+function MoviesPage(){
+
+  //TO DO : passer en props les objets de movies et mapper
+   const [movies, setMovies] = useState([]); 
+  //  const MOVIE_API="https://api.themoviedb.org/3/movie/550?api_key=b15f77463ee209064e6a74ac153c528c";
+  const MOVIE_API = " https://api.themoviedb.org/4/list/1?page=1&api_key=b15f77463ee209064e6a74ac153c528c&sort_by=original_order.asc";
+
+   useEffect(()=>{
+     fetch(MOVIE_API)
+       .then(res=> res.json())
+       .then((data)=>{
+         console.log(data);
+         setMovies(data.results);
+       });
+
+   },[]);
+   
+   console.log(movies, 'movies');
+   return (
+     <div>
+       {/* image à mettre */}
+       <div className='banniere'></div> 
+       <section className='affichage'>
+         {movies.map(movie=>(<Movie key={movie.id} movie={movie}/>))} 
+       </section>
+
+       <div className='trending'>Trending movies</div>
+
+       <div className='recent'>Recent movies</div>
+
+       <div className='top-rated'>Top-rated movies</div>
+     </div>
+  
+   );
+
+  return(
+    <div>page d'accueil</div>
+  )
+
+}
 
 export default MoviesPage;
