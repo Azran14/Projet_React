99f2d02d47ffd98df9acaa9323e61d17abd831d8
Azran14 <arnaud.dumarche@hetic.net>
2020-11-20 12:15:35 +0100

    rating

--- super-movie-searcher/src/Components/Movie/Movie.js
+++ super-movie-searcher/src/Components/Movie/Movie.js
@@ -1,9 +1,20 @@
 import React from "react";
+//import Rating from "../Rating/Rating.js";
 import "./Movie.css";
+import ReactStars from "react-rating-stars-component";
+// import RatingFull from "../../assets/RatingEmpty.svg";
+// import RatingEmpty from "../../assets/RatingFull.svg";
 
 const Movie = ({ movie }) => {
   // console.log(movie.poster_path, 'poster') //mettre un string
   const imgPath = "https://image.tmdb.org/t/p/original";
+  const rateParams = {
+    edit: false,
+    value: movie.vote_average / 2,
+    isHalf: true,
+    size: 25,
+  };
+
   return (
     <div className="Movie-card">
       <div className="Movie-imageContainer">
@@ -15,7 +26,7 @@ const Movie = ({ movie }) => {
       </div>
 
       <div className="Movie-infos">
-        <div className="Movie-rated">{movie.vote_average}</div>
+        <ReactStars className="Movie-rated" {...rateParams} />
         <h4 className="Movie-content">
           {movie.original_title} ({movie.release_date.substring(0, 4)})
         </h4>
--- super-movie-searcher/src/Components/MoviePage/MoviePage.js
+++ super-movie-searcher/src/Components/MoviePage/MoviePage.js
@@ -1,13 +1,14 @@
-import { useState, useEffect } from 'react';
-import './MoviePage.css';
-import { getMovie } from '../../service/movie/movie';
-import { imgPath } from '../../utils/constant';
+import { useState, useEffect } from "react";
+import "./MoviePage.css";
+import { getMovie } from "../../service/movie/movie";
+import ReactStars from "react-rating-stars-component";
+import { imgPath } from "../../utils/constant";
 
-import MovieCover from '../MovieCover/MovieCover';
+import MovieCover from "../MovieCover/MovieCover";
 
 const MoviePage = (id) => {
-  console.log(id.match.params.id, 'id');
-  const [movie, setMovie] = useState({});
+  console.log(id.match.params.id, "id");
+  const [movie, setMovie] = useState(null);
 
   useEffect(() => {
     getMovie(id.match.params.id).then((data) => {
@@ -15,41 +16,49 @@ const MoviePage = (id) => {
     });
   }, [id]);
 
-  console.log(movie, 'movie in movie page');
+  if (movie === null) {
+    return null;
+  }
+
+  const rateParams = {
+    edit: false,
+    isHalf: true,
+    value: movie.vote_average / 2,
+    size: 25,
+  };
+
   return (
     <main>
-      <section className='MoviePage-main'>
-        <div className='MoviePage-image'>
+      <section className="MoviePage-main">
+        <div className="MoviePage-image">
           <h2>{movie.title}</h2>
           <img src={imgPath + movie.poster_path} alt={movie.original_title} />
-          <div className='MoviePage-aside'>
-            <div className='MoviePage-content'>
-              rating: {movie.vote_average}
-            </div>
-            <p className='MoviePage-content'>
+          <div className="MoviePage-aside">
+            <ReactStars {...rateParams} />
+            <p className="MoviePage-content">
               Release date: {movie.release_date}
             </p>
             {/* <p className='MoviePage-content'>Runtime: {movie.runtime}</p> */}
           </div>
         </div>
-        <div className='MoviePage-body'>
-          <h3 className='MoviePage-subtitle'>Synopsis :</h3>
-          <p className='MoviePage-info'>{movie.overview}</p>
-          <h3 className='MoviePage-subtitle'>Genres :</h3>
-          <p className='MoviePage-info'>
-            {movie?.genres?.map((genre) => genre.name + ', ')}
+        <div className="MoviePage-body">
+          <h3 className="MoviePage-subtitle">Synopsis :</h3>
+          <p className="MoviePage-info">{movie.overview}</p>
+          <h3 className="MoviePage-subtitle">Genres :</h3>
+          <p className="MoviePage-info">
+            {movie?.genres?.map((genre) => genre.name + ", ")}
           </p>
-          <h3 className='MoviePage-subtitle'>Producers :</h3>
-          <p className='MoviePage-info'>
+          <h3 className="MoviePage-subtitle">Producers :</h3>
+          <p className="MoviePage-info">
             {movie?.production_companies?.map(
-              (production) => production.name + ' '
+              (production) => production.name + " "
             )}
           </p>
         </div>
       </section>
-      <section className='MoviePage-suggestion'>
-        <h3 className='MoviePage-subtitle'>You may like</h3>
-        <div className='MoviePage-container'>
+      <section className="MoviePage-suggestion">
+        <h3 className="MoviePage-subtitle">You may like</h3>
+        <div className="MoviePage-container">
           <MovieCover />
           <MovieCover />
           <MovieCover />
