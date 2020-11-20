e72a6eead9ee7fc96b51404780fb5a3e64fef3a3
Solid-Wolf <thomasliedts@gmail.com>
2020-11-20 10:31:46 +0100

    Fix search crash

--- super-movie-searcher/src/Components/Linedd/Linedd.js
+++ super-movie-searcher/src/Components/Linedd/Linedd.js
@@ -1,20 +1,20 @@
-import { Link } from "react-router-dom";
-import { imgPath } from "../../utils/constant";
+import { Link } from 'react-router-dom';
+import { imgPath } from '../../utils/constant';
 
-import "./Linedd.css";
+import './Linedd.css';
 
 function Linedd({ movie }) {
-  const url = "http://localhost:3000/";
+  const url = 'http://localhost:3000/';
   const currentUrl = window.location.href;
   console.log(currentUrl);
   return (
     <Link
-      className="Linedd"
+      className='Linedd'
       to={url === currentUrl ? `movie/${movie.id}` : `${movie.id}`}
     >
-      <img className="Linedd-img" src={imgPath + movie.poster_path} />
-      <h4 className="Linedd-info">
-        {movie.title}({movie.release_date.substring(0, 4)})
+      <img className='Linedd-img' src={imgPath + movie.poster_path} />
+      <h4 className='Linedd-info'>
+        {movie.title}({movie?.release_date?.substring(0, 4)})
       </h4>
     </Link>
   );
