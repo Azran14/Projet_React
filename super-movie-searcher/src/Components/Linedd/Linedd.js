417d5c6f63d219f50fcd38d014a252187c3e2236
Raphael <raphael.ferreira@hetic.net>
2020-11-20 09:46:10 +0100

    add img to dropdown items

--- super-movie-searcher/src/Components/Linedd/Linedd.js
+++ super-movie-searcher/src/Components/Linedd/Linedd.js
@@ -1,10 +1,12 @@
-import "./Linedd.css";
 import { Link } from "react-router-dom";
+import { imgPath } from "../../utils/constant";
+
+import "./Linedd.css";
 
 function Linedd({ movie }) {
   return (
     <Link className="Linedd" to={`movie/${movie.id}`}>
-      <div className="Linedd-img"></div>
+      <img className="Linedd-img" src={imgPath + movie.poster_path} />
       <h4 className="Linedd-info">
         {movie.title}({movie.release_date.substring(0, 4)})
       </h4>
