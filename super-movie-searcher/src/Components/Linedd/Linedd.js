c6ffc800080ddaf41604fee25fe1e8380a4241d1
Raphael <raphael.ferreira@hetic.net>
2020-11-20 09:29:03 +0100

    add link tag to dropdown elements

--- super-movie-searcher/src/Components/Linedd/Linedd.js
+++ super-movie-searcher/src/Components/Linedd/Linedd.js
@@ -1,15 +1,14 @@
 import "./Linedd.css";
+import { Link } from "react-router-dom";
 
 function Linedd({ movie }) {
-  console.log("loaded");
-
   return (
-    <div className="Linedd">
+    <Link className="Linedd" to={`movie/${movie.id}`}>
       <div className="Linedd-img"></div>
       <h4 className="Linedd-info">
         {movie.title}({movie.release_date.substring(0, 4)})
       </h4>
-    </div>
+    </Link>
   );
 }
 
